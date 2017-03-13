b551f2354276ee2bf5cd70216c1ac3157a28fd70
irenehuang189 <irenehuang189@gmail.com>
2017-03-14 01:30:43 +0700

    Create model for cinema

---
+++ Katoo/app/CinemaSchedule.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CinemaSchedule extends Model
+{
+    protected $table = 'cinema_schedule';
+}
---
+++ Katoo/app/CinemaTicketClass.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CinemaTicketClass extends Model
+{
+    protected $table = 'cinema_ticket_class';
+}
