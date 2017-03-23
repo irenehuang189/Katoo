ba161971b98de05e89200c8a559e3b3c60634976
Devina Ekawati <devina.ekawati@gmail.com>
2017-03-23 15:19:46 +0700

    Change request to json

--- ChatterBot/app.py
+++ ChatterBot/app.py
@@ -1,15 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from chatter_bot import ChatterBot
 
 app = Flask(__name__)
 cb = ChatterBot()
 
-@app.route('/get-reply', methods=['GET'])
+@app.route('/get-reply', methods=['POST'])
 def get_reply():
-    message = request.args.get('m')
-    reply = cb.get_reply(message)
-    return str(reply)
+	json = request.json
+	message = json["message"]
+	reply = cb.get_reply(message)
+	result = {'reply': str(reply)}
+	return jsonify(result)
 
-    
 if __name__ == '__main__':
     app.run()
