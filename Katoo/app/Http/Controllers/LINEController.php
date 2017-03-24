2e6b342319f2a769d8f32a715dd29446f38dff04
angelynz95 <angel_lynz95@hotmail.com>
2017-03-24 09:49:37 +0700

    fix LINEController

--- Katoo/app/Http/Controllers/LINEController.php
+++ Katoo/app/Http/Controllers/LINEController.php
@@ -10,23 +10,19 @@ use LINE\LINEBot\Event\MessageEvent\TextMessage;
 use LINE\LINEBot\Exception\InvalidEventRequestException;
 use LINE\LINEBot\Exception\InvalidSignatureException;
 use LINE\LINEBot\Exception\UnknownEventTypeException;
-use LINE\LINEBot\Exception\UnknownMessageTypeException; 
+use LINE\LINEBot\Exception\UnknownMessageTypeException;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 class LINEController extends Controller
 {
     private $bot;
 
     public function __construct() {
-        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(env('LINE_ACCESS_TOKEN', 'localhost'));
-        $this->bot = new \LINE\LINEBot($httpClient, ['channelSecret' => env('LINE_SECRET', 'localhost')]);
+        $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN', 'localhost'));
+        $this->bot = new LINEBot($httpClient, ['channelSecret' => env('LINE_SECRET', 'localhost')]);
     }
     
     public function index(Request $req) {
-        // echo $req;
-        // $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-        // $response = $this->bot->replyMessage('<replyToken>', $textMessageBuilder);
-
-        // echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
         $bot = $this->bot;
         $signature = $req->header(HTTPHeader::LINE_SIGNATURE);
         if (empty($signature)) {
@@ -35,11 +31,8 @@ class LINEController extends Controller
 
         // Check request with signature and parse request
         try {
-            // echo $req->getContent() . env('LINE_SECRET', 'localhost') . '++' . $signature;
-            // return;
             $events = $bot->parseEventRequest($req->getContent(), $signature);
         } catch (InvalidlidSignatureException $e) {
-            // return $e->getMessage();
             return response('Invalid signature', 400);
         } catch (UnknownEventTypeException $e) {
             return response('Unknown event type has come', 400);
@@ -61,13 +54,10 @@ class LINEController extends Controller
             }
 
             $replyText = $event->getText();
-            echo ('Reply text: ' . $replyText);
             $resp = $bot->replyText($event->getReplyToken(), $replyText);
-            echo ($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
         }
 
-        $res->write('OK');
-        return $res;
+        return ('OK');
     }
 
     public function getPopular() {
